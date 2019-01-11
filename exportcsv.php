0c6da55f61c2f97b2a8d53a92e5143daca145ed3
alspok <alspok@gmail.com>
2019-01-11 20:33:37 +0200

    exportcsv.php added

---
+++ exportcsv.php
@@ -0,0 +1,8 @@
+<form method='post' action=''>
+    Name: <input type='text' name='name'><br><br>
+    Email: <input type='text' name='email'><br><br>
+    Phone: <input type='text' name='phone'><br><br><br>
+    <input type='button' name='subscribe' value='Subscribe'>
+</form>
+
+<?php
